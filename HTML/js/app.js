42247b1c3aa9e80d57d07fbcda7ba953adda5dea
teajsshekokar <shekokar.tejas@gmail.com>
2018-01-29 22:56:50 +0530

    Add the HTML folder

---
+++ HTML/js/app.js
@@ -0,0 +1,29 @@
+
+var hostel = angular.module('hostel',['ngMaterial'])
+
+
+
+
+/*angular.module('hostel', ['ngMaterial']).controller('AppCtrl', function ($scope, $timeout, $mdSidenav) {
+    $scope.toggleLeft = buildToggler('left');
+    $scope.toggleRight = buildToggler('right');
+    
+             
+
+    function buildToggler(componentId) {
+      return function() {
+        $mdSidenav(componentId).toggle();
+      };
+    }
+  });
+
+hostel.config(function($routeprovider){
+              $routeprovider
+              .when("partials/Dashboard",
+                    {
+                    controller:'AppCtrl',
+                    templateUrl:'partials/Dashboard.html'
+                    }
+              )
+              
+              });*/
---
+++ HTML/js/controllers.js
@@ -0,0 +1,10 @@
+hostel.config(function($routeprovider){
+              $routeprovider
+              .when('/Dashboard'
+                    {
+                    controller:'AppCtrl',
+                    templateUrl:'partials/Dashboard.html'
+                    }
+              )
+              
+              });
